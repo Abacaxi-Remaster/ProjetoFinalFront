308f49d9799e1e1453d15ac9ade235ebd902fb15
LuEx10 <lucex109@gmail.com>
2023-05-25 19:36:12 -0300

    Hotfix após merges

--- lib/all.dart
+++ lib/all.dart
@@ -5,3 +5,9 @@ export '../Menus/MenuMentor.dart';
 //Pages
 export '../Pages/Cadastro.dart';
 export '../Pages/Login.dart';
+export '../Paginas/MenuAluno.dart';
+export '../Paginas/CrudTreinamentos.dart';
+export '../Paginas/MenuTestes.dart';
+export '../Paginas/MenuVagas.dart';
+export '../Paginas/MenuAtividadesConcluidas.dart';
+export '../Paginas/MenuADM.dart';

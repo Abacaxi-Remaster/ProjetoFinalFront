1d8f44c46e1960dfb42267d3422181aa672d9c66
LuEx10 <lucex109@gmail.com>
2023-05-27 19:16:39 -0300

    selecao menualuno antigo

--- lib/all.dart
+++ lib/all.dart
@@ -1,6 +1,5 @@
 export '../Paginas/Cadastro.dart';
 export '../Paginas/Login.dart';
-export '../Paginas/MenuAluno.dart';
 export '../Paginas/CrudTreinamentos.dart';
 export '../Paginas/MenuTestes.dart';
 export '../Paginas/MenuVagas.dart';

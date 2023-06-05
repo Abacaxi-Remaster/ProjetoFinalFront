f9a0107657bfaf6065112e93eb7184df287a1992
LuEx10 <lucex109@gmail.com>
2023-06-05 18:25:42 -0300

    deleção arquivo desnecessário

--- lib/all.dart
+++ lib/all.dart
@@ -3,5 +3,4 @@ export '../Paginas/Login.dart';
 export '../Paginas/CrudTreinamentos.dart';
 export '../Paginas/MenuTestes.dart';
 export '../Paginas/MenuAtividadesConcluidas.dart';
-export '../Paginas/MenuADM.dart';
 export 'index.dart';
